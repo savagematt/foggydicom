d4f526227872c98a04c4a52a13ff385ab2367175
Matt Savage <matthew.savage@gmail.com>
2020-08-16 22:39:30 +0100

    javafx ui

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -1,8 +1,15 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class HelloFX extends Application {
 
@@ -10,9 +17,34 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+
+        DirectoryChooser fileChooser = new DirectoryChooser();
+        Label from = new Label(" ");
+        Button fromButton = new Button("Read images from (folder or CD)");
+        fromButton.setOnAction(e -> {
+            File selectedFile = fileChooser.showDialog(stage);
+            from.setText(selectedFile.getAbsolutePath());
+        });
+
+        Label to = new Label(" ");
+        Button toButton = new Button("Save to directory");
+        toButton.setOnAction(e -> {
+            File selectedFile = fileChooser.showDialog(stage);
+            to.setText(selectedFile.getAbsolutePath());
+        });
+
+        Button goButton = new Button("GO");
+
+        VBox box = new VBox(
+            fromButton, from,
+            toButton,
+            to,
+            goButton);
+        box.setSpacing(10);
+        box.setPadding(new Insets(10));
+        Scene scene = new Scene(box, 640, 480);
         stage.setScene(scene);
+        stage.sizeToScene();
         stage.show();
     }
 
